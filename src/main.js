5c716514f01eda9c0031fd23c02bdd78446ccbe0
xiumubai <1547702880@qq.com>
2022-08-31 17:40:24 +0800

    feat: 引入element-ui

--- src/main.js
+++ src/main.js
@@ -1,33 +1,44 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from '@/router'
-import TypeNav from '@/components/TypeNav'
-import Pagination from '@/components/Pagination'
-import store from './store'
-import './mock/mockServer'
-import './plugins'
-import * as API from '@/api'
+/*
+ * @Author: 朽木白
+ * @Date: 2022-08-30 08:27:15
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-31 16:28:30
+ * @Description:
+ */
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import TypeNav from '@/components/TypeNav';
+import Pagination from '@/components/Pagination';
+import store from './store';
+import './mock/mockServer';
+import './plugins';
+import * as API from '@/api';
 
-import '@/components/AddressModel'
+import '@/components/AddressModel';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.prototype.$API = API // 将包含所有接口请求函数的对象保存到Vue原型对象
+Vue.prototype.$API = API; // 将包含所有接口请求函数的对象保存到Vue原型对象
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 // 注册全局组件
-Vue.component(TypeNav.name, TypeNav)
-Vue.component(Pagination.name, Pagination)
+Vue.component(TypeNav.name, TypeNav);
+Vue.component(Pagination.name, Pagination);
 
 // 创建一个vm作为总线对象挂到Vue原型对象上
 // Vue.prototype.$bus = new Vue()
 
 new Vue({
-  beforeCreate () {
+  beforeCreate() {
     // 将当前vm作为总线对象挂到Vue原型对象上
-    Vue.prototype.$bus = this
+    Vue.prototype.$bus = this;
   },
-  
-  render: h => h(App),
+
+  render: (h) => h(App),
   router, // 路由器
   store, // vuex的store
-}).$mount('#app')
+}).$mount('#app');
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: 朽木白
+ * @Date: 2022-08-30 08:27:15
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-08-31 16:29:10
+ * @Description: 
+-->
 <template>
   <div>
     <!-- 大写转小写, 中间用-连接 -->
@@ -11,49 +18,46 @@
     <!-- 猜你喜欢 -->
     <Like />
     <!--楼层-->
-    <Floor v-for="floor in floors" :floor="floor" :key="floor.id"/>
+    <Floor v-for="floor in floors" :floor="floor" :key="floor.id" />
     <!--商标-->
     <Brand />
   </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex'
-  import Brand from './Brand/Brand'
-  import Floor from './Floor/Floor'
-  import Like from './Like/Like'
-  import ListContainer from './ListContainer/ListContainer'
-  import Rank from './Rank/Rank'
-  import TodayRecommend from './TodayRecommend/TodayRecommend'
-  // import {reqBanners, reqFloors} from '@/api'
+import { mapState } from 'vuex';
+import Brand from './Brand/Brand';
+import Floor from './Floor/Floor';
+import Like from './Like/Like';
+import ListContainer from './ListContainer/ListContainer';
+import Rank from './Rank/Rank';
+import TodayRecommend from './TodayRecommend/TodayRecommend';
+// import {reqBanners, reqFloors} from '@/api'
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
+  computed: {
+    ...mapState({
+      floors: (state) => state.home.floors, // [] ==> [{carouselList: []}, {}]
+    }),
+  },
 
-    computed: {
-      ...mapState({
-        floors: state => state.home.floors,  // [] ==> [{carouselList: []}, {}]
-      })
-    },
+  async mounted() {
+    this.$store.dispatch('getBanners');
+    this.$store.dispatch('getRecommends');
+    this.$store.dispatch('getFloors');
+  },
 
-    async mounted () {
-      this.$store.dispatch('getBanners')
-      this.$store.dispatch('getRecommends')
-      this.$store.dispatch('getFloors')
-    },
-
-    components: {
-      Brand,
-      Floor,
-      Like,
-      ListContainer,
-      Rank,
-      TodayRecommend,
-    }
-  }
+  components: {
+    Brand,
+    Floor,
+    Like,
+    ListContainer,
+    Rank,
+    TodayRecommend,
+  },
+};
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
